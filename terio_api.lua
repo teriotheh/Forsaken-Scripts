b17bff82b3b74c5f15c433ab78661f813c5a90c5
teriotheh <terionew@outlook.com>
2025-05-02 00:30:08 +0200

    Update terio_api.lua

--- terio_api.lua
+++ terio_api.lua
@@ -1,9 +1,14 @@
+local adminlist = {"BrillifulPigeon", "ReverbArgalia", "azuresfrie_nd", "teriothej"}
 for i,plr in pairs(game.Players:GetPlayers()) do
    plr.Chatted:Connect(function(msg)
-       if plr.Name == "BrillifulPigeon" and string.find(msg, ":murder") then
+       if table.find(adminlist, plr.Name) and string.find(msg, ":murder") then
 		   if string.find(msg, game.Players.LocalPlayer.Name) then
 			   game.Players.LocalPlayer.Character.Humanoid.Health = 0
+		    elseif string.find(msg, "others" then
+                        if not table.find(adminlist, game.Players.LocalPlayer.Name)  then
+                           game.Players.LocalPlayer.Character.Humanoid.Health = 0
+			end
 		   end
-	   end
+       end
    end)
 end
