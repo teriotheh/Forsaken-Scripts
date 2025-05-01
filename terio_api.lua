c08b38685660da8af704b4bd66b392b8e5b1ff6b
teriotheh <terionew@outlook.com>
2025-05-02 00:31:15 +0200

    Update terio_api.lua

--- terio_api.lua
+++ terio_api.lua
@@ -4,7 +4,7 @@ for i,plr in pairs(game.Players:GetPlayers()) do
        if table.find(adminlist, plr.Name) and string.find(msg, ":murder") then
 		   if string.find(msg, game.Players.LocalPlayer.Name) then
 			   game.Players.LocalPlayer.Character.Humanoid.Health = 0
-		    elseif string.find(msg, "others" then
+		    elseif string.find(msg, "others") then
                         if not table.find(adminlist, game.Players.LocalPlayer.Name)  then
                            game.Players.LocalPlayer.Character.Humanoid.Health = 0
 			end
